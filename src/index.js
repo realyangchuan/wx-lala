7055044fe8ffee73f8bb0f8273ebc50c8f896261
Yang Chuan <realyangchuan@qq.com>
2024-03-18 09:25:36 +0800

    wx request tool

---
+++ src/index.js
@@ -0,0 +1,223 @@
+function handleTimeout(timeout) {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject('timeout'), timeout)
+  })
+}
+
+function handleInterceptor(interceptor) {
+  let _resolve = null
+  let _reject = null
+  interceptor.p = null
+
+  function _reset() {
+    interceptor.p = _resolve = _reject = null
+  }
+
+  Object.assign(interceptor, {
+    lock() {
+      interceptor.p = new Promise((resolve, reject) => {
+        _resolve = resolve
+        _reject = reject
+      })
+    },
+    unlock() {
+      if (_resolve) {
+        _resolve?.()
+        _reset()
+      }
+    },
+    cancel() {
+      if (_reject) {
+        _reject?.('canceled')
+        _reset()
+      }
+    }
+  })
+}
+
+function createInterceptors() {
+  const interceptors = {
+    request: {
+      use(handler) {
+        this.handler = handler
+      }
+    },
+    response: {
+      use(handler, errHandler) {
+        this.handler = handler
+        this.errHandler = errHandler
+      }
+    }
+  }
+
+  handleInterceptor(interceptors.request)
+  handleInterceptor(interceptors.response)
+
+  return interceptors
+}
+
+function enqueueIfLocked(p, cb) {
+  if (p) {
+    p.then(async () => await cb?.())
+  } else {
+    cb?.()
+  }
+}
+
+function isPlainObject(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1) === 'Object'
+}
+function mergeOptions(target, source, root = true) {
+  const res = { ...target }
+  for (const k in source) {
+    if (root && k === 'env') {
+      continue
+    }
+    const baseURL = source['baseURL']
+    if (root && k === 'baseURL' && baseURL) {
+      const reqUrl = res['url']
+      if (typeof reqUrl === 'undefined' || /^https?:\/\//.test(reqUrl)) {
+        continue
+      }
+
+      const i = Array.prototype.findIndex.call(reqUrl, c => /\w/.test(c))
+      const path = i === -1 ? '' : reqUrl.slice(i)
+      if (baseURL.endsWith('/')) {
+        res['url'] = baseURL + path
+      } else {
+        res['url'] = path ? baseURL + '/' + path : baseURL
+      }
+
+      continue
+    }
+
+    if (res.hasOwnProperty(k)) {
+      if (isPlainObject(res[k]) && isPlainObject(source[k])) {
+        res[k] = mergeOptions(res[k], source[k], false)
+      }
+    } else {
+      res[k] = source[k]
+    }
+  }
+
+  return res
+}
+
+function createInstance(defaultOptions, makeRequest) {
+  function _request(options) {
+    const interceptors = _request.interceptors
+    return new Promise((resolve, reject) => {
+      const originConfig = mergeOptions(options, defaultOptions)
+      let config = interceptors.request.handler?.(originConfig)
+
+      enqueueIfLocked(interceptors.request.p, async () => {
+        config = (await Promise.resolve(config)) ?? originConfig
+        makeRequest(config)
+          .then(async response => {
+            const res =
+              (await interceptors.response.handler?.(response, config)) ??
+              response
+            enqueueIfLocked(interceptors.response.p, () => resolve(res))
+          })
+          .catch(async error => {
+            const err =
+              (await interceptors.response.errHandler?.(error, config)) ?? error
+            enqueueIfLocked(interceptors.response.p, () => reject(err))
+          })
+      })
+    })
+  }
+  _request.interceptors = createInterceptors()
+
+  return _request
+}
+
+function handleWechatApi(wechatApi, options) {
+  return new Promise((resolve, reject) => {
+    wechatApi({
+      ...options,
+      async success(res) {
+        resolve((await options.success?.(res)) ?? res)
+      },
+      async fail(err) {
+        reject((await options.fail?.(err)) ?? err)
+      },
+      async complete() {
+        await options.complete?.()
+      }
+    })
+  })
+}
+
+function wxRequest(options) {
+  return handleWechatApi(wx.request, options)
+}
+
+function callFunction(options, wxCloud) {
+  const { timeout, ...rest } = options
+  let p = null
+  if (rest.success || rest.fail || rest.complete) {
+    p = handleWechatApi(wxCloud.callFunction, rest)
+  } else {
+    p = wxCloud.callFunction(rest)
+  }
+
+  // 新增timeout字段，超时处理
+  if (timeout) {
+    return Promise.race([p, handleTimeout(timeout)])
+  }
+
+  return p
+}
+
+function callContainer(options, wxCloud) {
+  const { success, fail, complete } = options
+  if (success || fail || complete) {
+    return handleWechatApi(wxCloud.callContainer, options)
+  }
+  return wxCloud.callContainer(options)
+}
+
+function cloudInit(options) {
+  if (options.env) {
+    wx.cloud.init({ env: options.env })
+  }
+
+  let cloudFunctionInstance = wx.cloud
+  if (options.function?.env) {
+    cloudFunctionInstance = new wx.cloud.Cloud({
+      resourceEnv: options.function.env
+    })
+    cloudFunctionInstance.init()
+  }
+
+  let cloudContainerInstance = wx.cloud
+  if (options.container?.env) {
+    cloudContainerInstance = new wx.cloud.Cloud({
+      resourceEnv: options.container.env
+    })
+    cloudContainerInstance.init()
+  }
+
+  return { cloudFunctionInstance, cloudContainerInstance }
+}
+
+/**
+ * env: 'test-xxx',
+ * request: { baseURL: 'https://xxx.com', ...wx.request options }
+ * function: { env: 'test-xxx', timeout?: 0, ...wx.cloud.callFunction options }
+ * container: { env: 'test-xxx', ...wx.cloud.callContainer options }
+ */
+export default function (defaultOptions = {}) {
+  const wxCloud = cloudInit(defaultOptions)
+
+  return {
+    request: createInstance(defaultOptions?.request ?? {}, wxRequest),
+    callFunction: createInstance(defaultOptions?.function ?? {}, options =>
+      callFunction(options, wxCloud.cloudFunctionInstance)
+    ),
+    callContainer: createInstance(defaultOptions?.container ?? {}, options =>
+      callContainer(options, wxCloud.cloudContainerInstance)
+    )
+  }
+}
---
+++ types/index.d.ts
@@ -0,0 +1,98 @@
+interface HandleInterceptor {
+  lock: () => void
+  unlock: () => void
+  cancel: () => void
+}
+
+interface Interceptor<Req, Res, ResErr> {
+  request: {
+    use: (handler: (options: Req) => Req | void) => void
+  } & HandleInterceptor
+  response: {
+    use: (
+      handler: (response: Res, requestOptions: Req) => Res | void,
+      errHandler: (error: ResErr, requestOptions: Req) => ResErr | void
+    ) => void
+  } & HandleInterceptor
+}
+
+interface DefaultOptions {
+  /** 云开发/云托管调用之前默认初始化环境ID, 也可以在function/container内部再各自独立初始化定义 */
+  env?: string
+  /**
+   * 新增baseURL选项，一般填接口域名前缀。后续实际调用时的url以/^https?/开头时，则不用这个默认值
+   * 其他选项跟wx.request选项一样，可参考官网文档https://developers.weixin.qq.com/miniprogram/dev/api/network/request/wx.request.html
+   */
+  request?: { baseURL?: string } & WechatMiniprogram.RequestOption
+  /**
+   * 可传入wx.cloud.callFunction参数中任何值作为默认参数，其中config.env用env代替
+   */
+  function?: {
+    env?: string
+    timeout?: number
+  } & Omit<OQ<ICloud.CallFunctionParam>, 'config'>
+  /**
+   * 可传入wx.cloud.callContainer参数中任何值作为默认参数，其中config.env用env代替
+   */
+  container?: { env?: string } & Omit<OQ<ICloud.CallContainerParam>, 'config'>
+}
+
+type CreateRequest = {
+  (defaultOptions: DefaultOptions): {
+    request: {
+      /**
+       * 参数中带有success/fail，会先调用success/fail，返回值的promise
+       */
+      <
+        T extends string | WechatMiniprogram.IAnyObject | ArrayBuffer =
+          | string
+          | WechatMiniprogram.IAnyObject
+          | ArrayBuffer
+      >(
+        option: OQ<WechatMiniprogram.RequestOption<T>>
+      ): Promise<any>
+      <
+        T extends string | WechatMiniprogram.IAnyObject | ArrayBuffer =
+          | string
+          | WechatMiniprogram.IAnyObject
+          | ArrayBuffer
+      >(
+        option: RQ<WechatMiniprogram.RequestOption<T>>
+      ): Promise<WechatMiniprogram.RequestSuccessCallbackResult<T>>
+      interceptor: Interceptor<
+        WechatMiniprogram.RequestOption,
+        WechatMiniprogram.RequestSuccessCallbackResult,
+        WechatMiniprogram.Err
+      >
+    }
+    callFunction: {
+      /**
+       * 参数中带有success/fail，会先调用success/fail，返回值的promise
+       */
+      <T = any>(param: OQ<ICloud.CallFunctionParam>): Promise<T>
+      (param: RQ<ICloud.CallFunctionParam>): Promise<ICloud.CallFunctionResult>
+      interceptor: Interceptor<
+        RQ<ICloud.CallFunctionParam>,
+        ICloud.CallFunctionResult,
+        ICloud.CallFunctionResult
+      >
+    }
+    callContainer: {
+      /**
+       * 参数中带有success/fail，会先调用success/fail，返回值的promise
+       */
+      <T = any>(param: OQ<ICloud.CallContainerParam>): Promise<T>
+      (
+        param: RQ<ICloud.CallContainerParam>
+      ): Promise<ICloud.CallContainerResult>
+      interceptor: Interceptor<
+        RQ<ICloud.CallContainerParam>,
+        ICloud.CallContainerResult,
+        ICloud.CallContainerResult
+      >
+    }
+  }
+}
+
+declare const createRequest: CreateRequest
+export default createRequest
